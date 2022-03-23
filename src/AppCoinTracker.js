6acfb7776f7611c4cab379df4dcfe85ef394ba0d
KKH <kyunghun96@gmail.com>
2022-03-23 17:13:57 +0900

    Ex: Coin Converter

---
+++ src/AppCoinTracker.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+function AppCoinTracker() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, [coins]);
+
+  const onChangeSelect = (event) => {
+    setSelected((prev) => {
+      return 20 / event.target.value;
+    });
+  };
+
+  return (
+    <div>
+      <h1>The Coins ! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onChangeSelect}>
+            {coins.map((coin, index) => (
+              <option key={index} value={Math.round(coin.quotes.USD.price)}>
+                {coin.name} ({coin.symbol}): {Math.round(coin.quotes.USD.price)}{" "}
+                USD
+              </option>
+            ))}
+          </select>
+          <input
+            type="text"
+            placeholder="Select coin to be Converted..."
+            value={selected}
+            readOnly
+          ></input>
+          <ul>
+            {coins.map((coin, index) => (
+              <li key={index}>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AppCoinTracker;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import AppTodoList from "./App_TodoList";
+import AppCoinTracker from "./AppCoinTracker";
 //import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <AppTodoList />
+    <AppCoinTracker />
   </React.StrictMode>,
   document.getElementById("root")
 );
